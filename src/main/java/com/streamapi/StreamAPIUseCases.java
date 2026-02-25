d1d639493e2e0b1b8cd657ffc0d6a450ece90e0a
Aniket Mishra <aniketmishrarr@gmail.com>
2026-02-25 14:00:31 +0530

    [Aniket]: feat [stream-api]: add min and max operations

--- src/main/java/com/streamapi/StreamAPIUseCases.java
+++ src/main/java/com/streamapi/StreamAPIUseCases.java
@@ -2,10 +2,8 @@ package com.streamapi;
 import java.util.*;
 public class StreamAPIUseCases {
     public static void main(String[] args){
-        System.out.println(
-            Arrays.asList(1,3,5,6,8)
-            .stream().filter(n -> n % 2 == 0)
-            .findFirst().orElse(-1)
-        );
+        List<Integer> n = Arrays.asList(5,2,9,1,7);
+        System.out.println(n.stream().min(Integer::compare).get());
+        System.out.println(n.stream().max(Integer::compare).get());
     }
 }
